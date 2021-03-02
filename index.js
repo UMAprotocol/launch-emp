92063c4bc5bf0d217db498874044a798046ad47b
John Shutt <john.d.shutt@gmail.com>
2021-03-02 14:09:16 -0800

    improve(params): make minSponsorTokens a parameter passed as an argument
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const libraryAddress = argv.libraryAddress ? argv.libraryAddress : "0x0000000000
     disputeBondPercentage: { rawValue: toWei("0.1") }, // 10% dispute bond.
     sponsorDisputeRewardPercentage: { rawValue: toWei("0.05") }, // 5% reward for sponsors who are disputed invalidly
     disputerDisputeRewardPercentage: { rawValue: toWei("0.2") }, // 20% reward for correct disputes.
-    minSponsorTokens: { rawValue: toWei("100") }, // Min sponsor position size of 100 synthetic tokens.
+    minSponsorTokens: { rawValue: toWei(argv.minSponsorTokens.toString()) }, // Minimum sponsor position size.
     liquidationLiveness: 7200, // 2 hour liquidation liveness.
     withdrawalLiveness: 7200, // 2 hour withdrawal liveness.
     financialProductLibraryAddress: libraryAddress, // Default to 0x0 if no address is passed.
